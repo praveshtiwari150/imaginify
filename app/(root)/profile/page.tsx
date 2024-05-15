fc09d5dfb493c1ecb83ada2ce5f9689dc5bc82d7
praveshtiwari150 <praveshtiwari150@gmail.com>
2024-05-16 02:39:46 +0530

    initial commit

---
+++ app/(root)/profile/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProfilePage = () => {
+  return (
+    <div>
+      profiel
+    </div>
+  )
+}
+
+export default ProfilePage
---
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddTransformationTypePage = () => {
+  return (
+    <div>
+      add transformationpage
+    </div>
+  )
+}
+
+export default AddTransformationTypePage;
